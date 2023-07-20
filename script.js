24f6e0756b7534eb637194981abd98dc1dd32e2c
Ravuri Teja Nagaswi <111522542+Tejaswi0418@users.noreply.github.com>
2023-07-20 20:25:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const button =document.querySelector("button");
+const body = document.querySelector("body");
+const color=['violet','indigo','blue','green','yello','orange','red'];
+body.style.backgroundColor='pink';
+document.addEventListener('click',function(){;
+    const colorIndex=parseInt(Math.random()*color.length);
+    body.style.backgroundColor=color[colorIndex];
+});
+const numbers=['1','2','3','4','5','6','7'];
+document.addEventListener('click',function(){;
+Math.floor.getElementById("b1").innerHTML=Math.floor(math.random()*numbers.length);
+
+})
